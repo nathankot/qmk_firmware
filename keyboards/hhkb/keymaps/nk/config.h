f047d916f28e3db7e13ff335eae2e42c54746925
Nathan Kot <me@nathankot.com>
2017-11-22 17:20:24 +0900

    Tweak settings

--- keyboards/hhkb/keymaps/nk/config.h
+++ keyboards/hhkb/keymaps/nk/config.h
@@ -72,7 +72,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
-#define TAPPING_TERM 150
-#define PERMISSIVE_HOLD
+#define TAPPING_TERM 120
+#define QMK_KEYS_PER_SCAN 4
 
 #endif
