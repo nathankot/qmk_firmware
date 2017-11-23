b76631e129344ee273d5d718955b700028a47eb1
Nathan Kot <me@nathankot.com>
2017-11-24 01:56:40 +0900

    Update config again

--- keyboards/hhkb/keymaps/nk/config.h
+++ keyboards/hhkb/keymaps/nk/config.h
@@ -72,8 +72,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
 
-#define TAPPING_TERM 120
-#define QMK_KEYS_PER_SCAN 4
+#define TAPPING_TERM 100
 #define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
 
 #endif
