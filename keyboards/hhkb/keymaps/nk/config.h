749e9a25d1f31db7c83fe04dfb7c7949e4f4fc0c
Nathan Kot <me@nathankot.com>
2018-05-16 01:07:15 +0900

    Enable IGNORE_MOD_TAP_INTERRUPT

--- keyboards/hhkb/keymaps/nk/config.h
+++ keyboards/hhkb/keymaps/nk/config.h
@@ -75,7 +75,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAPPING_TERM 80
 #define PERMISSIVE_HOLD
-/* #define IGNORE_MOD_TAP_INTERRUPT */
+#define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_FORCE_HOLD
 
 #endif
