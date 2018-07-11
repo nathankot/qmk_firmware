f8e2735825b6bda41b7d5bdd6323b90bab28ed9a
Nathan Kot <me@nathankot.com>
2018-07-11 21:14:51 +0900

    Updates

--- keyboards/pearl/keymaps/nk/keymap.c
+++ keyboards/pearl/keymaps/nk/keymap.c
@@ -56,7 +56,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [SC2_CG_LAYER] = LAYOUT_all(
     KC_TRNS,                   KC_1,              KC_2,                  KC_3,                KC_4,                    KC_5,     KC_6,           KC_7,    KC_8,     KC_9,     KC_TRNS,   KC_TRNS,            KC_TRNS,     \
-    KC_TRNS,                   KC_TRNS,           KC_TRNS,               KC_TRNS,             KC_TRNS,                 KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            \
+    KC_TRNS,                   KC_TRNS,           KC_0,                  KC_9,                KC_8,                    KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            \
     KC_TRNS,                   KC_TRNS,           KC_TRNS,               KC_TRNS,             KC_TRNS,                 KC_TRNS,  KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS,   KC_TRNS,            \
     KC_TRNS,                   KC_LGUI,           KC_TRNS,               KC_TRNS,             KC_TRNS,                 KC_TRNS,  KC_TRNS,        KC_TRNS \
   ),
